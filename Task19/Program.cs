b6b848f8ed60ebd15a32120e1f46a2f3f643c408
Lakosta <8bit92@gmail.com>
2022-06-19 19:43:44 +0300

    Add Task19 and Task21

---
+++ Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Clear();
+
+Console.WriteLine("Insert number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+int copy = num;
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Error!!!");
+}
+
+else
+{
+    while (copy > 0)
+    {
+        num2 = num2 * 10 + copy % 10;
+        copy = copy / 10;
+    }
+    if (num == num2)
+    {
+        Console.WriteLine("Polindrom");
+    }
+    else
+    {
+        Console.WriteLine("Not polindrom");
+    }
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты x1");
+double x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y1");
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты z1");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты x2");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y2");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты z2");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double lineX = x1 - x2;
+double lineY = y1 - y2;
+double lineZ = z1 - z2;
+
+double sum = Math.Sqrt((lineX*lineX) + (lineY * lineY) + (lineZ * lineZ));
+
+Console.WriteLine($"Расстояние между точками = {sum}");
